b05c91ecd74f0dc91a90c35174aa802ace5c95fb
NikolayM17 <64014097+NikolayM17@users.noreply.github.com>
2020-09-03 11:37:22 +0300

    Add files via upload

---
+++ Алгоритмы. Входной контроль/if 15.py
@@ -0,0 +1,16 @@
+#   If15. Даны три числа. Найти сумму двух наибольших из них.
+
+
+a = int(input())
+
+b = int(input())
+
+c = int(input())
+
+sum = 0
+
+sum = a + b + c
+
+sum -= min(a, b, c)
+
+print(sum)
