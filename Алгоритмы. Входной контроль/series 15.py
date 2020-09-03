09f53868299e7d6ca271f8606f7796a50ac55257
NikolayM17 <64014097+NikolayM17@users.noreply.github.com>
2020-09-03 12:22:26 +0300

    Add files via upload

---
+++ Алгоритмы. Входной контроль/series 15.py
@@ -0,0 +1,26 @@
+''' Series15∞. ƒано целое число K и набор ненулевых целых чисел;
+признак его завершени€ Ч число 0. ¬ывести номер первого числа в наборе, большего K.
+≈сли таких чисел нет, то вывести 0. '''
+
+
+k = int(input())
+
+k_rem = k
+
+number = 1
+
+while number != 0:
+
+    number = int(input())
+
+    if k < number:
+
+        print (number)
+
+        k = number
+
+        break
+
+if k_rem == k:
+
+    print(0)
